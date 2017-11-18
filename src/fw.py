e28f7cc7b84abae0edf21761da4fb5dbb59b82e0
mfalthaw <mfalthaw@ucalgary.ca>
2017-11-18 00:20:13 -0700

    parse arguments

--- src/fw.py
+++ src/fw.py
@@ -1,2 +1,26 @@
 #!/usr/bin/env python
 ''' fw .py '''
+
+import sys
+import string
+import argparse
+
+def parse_args():
+    '''
+    Handles parsing arguments and returning arugments list
+    Reference: https://docs.python.org/3/library/argparse.h
+    '''
+    usage = 'python3 fw.py rules_filename'
+    parser = argparse.ArgumentParser(usage=usage)
+
+    parser.add_argument('rules_filename', 
+                        help='firewall configuration file',
+                        type=str)
+    
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+
+if __name__ == '__main__':
+    main()
