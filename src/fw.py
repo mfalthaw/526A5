cb492e2dd5d017c241e42f151e173cbd05efb9e2
mfalthaw <mfalthaw@ucalgary.ca>
2017-11-18 00:32:51 -0700

    read config file

--- src/fw.py
+++ src/fw.py
@@ -1,9 +1,31 @@
 #!/usr/bin/env python
-''' fw .py '''
+''' fw.py '''
 
 import sys
 import string
 import argparse
+
+def create_rule(line):
+    '''
+    create a rule for each configuration line
+    @param line: each line must have 4 to 5 items
+    line format: <direction> <action> <ip> <port> [flag]
+    '''
+
+
+def read_configs(filename):
+    '''
+    read config file
+    '''
+    with open(filename) as file:
+        for line in file:
+            line = line.strip()
+            line = line.split()
+            
+            create_rule(line)
+
+    file.close()    
+    print('Done reading config file: {}'.format(filename))
 
 def parse_args():
     '''
