724c30a23490483a1fc8f0d61210a6b7e8ec7a36
Moath <mfalthaw@ucalgary.ca>
2017-11-18 13:17:11 -0700

    input vlidation

--- src/fw.py
+++ src/fw.py
@@ -15,15 +15,43 @@ def create_rule(items):
     line format: <direction> <action> <ip> <port> [flag]
     '''
     rule = {}
-    
+
     if len(items) in (4, 5):
-        rule['direction'] = items[0].lower()
-        rule['action'] = items[1].lower()
-        rule['ip'] = items[2]
-        rule['port'] = items[3]
+        direction = items[0].lower()
+        if direction in ('in', 'out'):
+            rule['direction'] = direction
+        else:
+            raise ValueError('Error: direction unrecogonized.')
+        
+        action = items[1].lower()
+        if action in ('accept', 'reject', 'drop'):
+            rule['action'] = action
+        else:
+            raise ValueError('Error: action unrecogonized.')
+        
+        ip = items[2]
+        if (ip == '*') \
+            or (ip.count('.') == 3) \
+            or (ip.count('.') == 3  and ip.count('/') == 1):
+            rule['ip'] = ip
+        else:
+            raise ValueError('Error: invalid ip.')
+
+        port = items[3]
+        if (port == '*') \
+            or (port in range(0, 65536)):
+            # TODO handle multiple ports
+            rule['port'] = port
+        else:
+            raise ValueError('Error: invalid port.')
+
         if len(items) == 5:
             # if flag used
-            rule['flag'] = items[4]
+            flag = items[4].lower()
+            if flag == 'established':
+                rule['flag'] = flag
+            else:
+                raise ValueError('Error: invalid flag.')
         else:
             rule['flag'] = None
     # unsupported length
@@ -43,8 +71,10 @@ def read_configs(filename):
         for line in file:
             line = line.strip()
             items = line.split()
-            
-            rules.append(create_rule(items))
+            try:
+                rules.append(create_rule(items))
+            except ValueError as e:
+                print(e)
 
     file.close()    
     print('Done reading config file: {}'.format(filename))
@@ -66,6 +96,7 @@ def parse_args():
 def main():
     args = parse_args()
     read_configs(args.rules_filename)
+    print(rules)
 
 if __name__ == '__main__':
     main()
